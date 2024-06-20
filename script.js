436edae75951737b85289d1161ecf4fbb05e70db
amonsusmom <mohamedyaiche13511@gmail.com>
2024-06-20 12:35:01 +0100

    hello

---
+++ script.js
@@ -0,0 +1,39 @@
+let myname = "ibtihel"
+myname ="helllo"
+console.log(myname)
+const num= 52.5
+console.log(num * 6)
+console.log(7==9)
+let x = null
+console.log(x)
+const names = ["youssef", "esra", "asma", "nour"]
+
+const person = {
+    firstName:"yasmine",
+    lastName: "Tebourski",
+    school: 1
+}
+console.log(person)
+console.log(person.lastName)
+ 
+const students = [
+    {
+        firstName:"yasmine",
+        lastName: "Tebourski",
+        school: 1
+    },{
+        firstName:"dhia",
+        lastName: "dachraoui",
+        school: 5
+    },{
+        firstName:"khalil",
+        lastName: "ben kram",
+        school: 4
+    }
+]
+alert("hi")
+var visitorName  = prompt("What's your name : ")
+const eye = 20
+if( students[1].firstname == "yasmine"){console.log(true)
+}else{console.log(false)}
+
